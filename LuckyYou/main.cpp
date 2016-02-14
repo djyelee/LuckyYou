4b47d5f3ac6e9c8b358b33817cf166be3153c494
DJ Lee <djlee@byu.edu>
2016-02-13 22:11:09 -0700

    Initial Commit

---
+++ LuckyYou/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LuckyYou
+//
+//  Created by DJ Lee on 2/13/16.
+//  Copyright © 2016 DJ Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
